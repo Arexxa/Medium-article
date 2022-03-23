b5f621030cef28039e65e5e52f68ccd1310eaa19
Shahrulnizam <shahrulayol31@gmail.com>
2022-03-23 15:29:01 +0800

    add new component

---
+++ src/components/CommentBox.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="form">
+    <h2>Comment Section</h2>
+    <input type="text" placeholder="Name" v-model="name" /><br>
+    <input type="text" placeholder="Comment" v-model="newComment" /><br>
+    <button @click="addComment" class="submit">Submit</button>
+  </div>
+
+  <h2 v-for="(comment, index) in comments" :key="index">{{ comment }}</h2>
+</template>
+
+<script>
+
+export default {
+    name: 'CommentBox',
+    data() {
+        return {
+            name: "",
+            newComment: "",
+            comments: []
+        }
+    },
+
+    mounted() {
+        this.loadComments('23')
+    },
+
+    methods: {
+      addComment() {
+        if(this.newComment){
+          this.comments.push({
+            comment: this.newComment,
+          });
+          this.newComment = "";
+        }
+      }
+    },
+
+    loadComments(id) {
+        //load
+    }
+}
+</script>
+
+<style>
+
+.form {
+  padding: 30px;
+  background: #a9c9ba;
+  margin: 10px auto;
+  max-width: 600px;
+}
+
+.form input{
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  color: #444;
+}
+
+.form button {
+  padding: 5px;
+  border-radius: 10px;
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  border-radius: 2px;
+}
+
+</style>
--- src/views/Post-details.vue
+++ src/views/Post-details.vue
@@ -5,63 +5,30 @@
   <h3>{{ title }}</h3>
   <p>{{ details }}</p>
 
-  <div class="form">
-    <h2>Comment Section</h2>
-    <input type="text" placeholder="Name" v-model="name" /><br>
-    <input type="text" placeholder="Comment" v-model="comment" /><br>
-    <button @click="addComment" class="submit">Submit</button>
-  </div>
+  <comment-box ></comment-box>
+  
 </template>
 
 <script>
+import CommentBox from '../components/CommentBox.vue'
+
 export default {
     name: 'PostDetails',
     props: ['id'],
+    components: {
+      CommentBox
+    },
+
     data() {
         return {
             id: this.$route.params.id,
             title: this.$route.params.title,
             details: this.$route.params.details,
-            name: "",
-            comment: "",
         }
-    },
-    methods: {
-      addComment() {
-        if(this.comment){
-          this.comment.push({
-            comment: this.comment,
-          });
-          this.comment = "";
-        }
-      }
     }
 }
 </script>
 
 <style>
-.form {
-  padding: 30px;
-  background: #a9c9ba;
-  margin: 10px auto;
-  max-width: 600px;
-}
 
-.form input{
-  background: #f4f4f4;
-  padding: 20px;
-  border-radius: 10px;
-  margin: 10px auto;
-  max-width: 600px;
-  color: #444;
-}
-
-.form button {
-  padding: 5px;
-  border-radius: 10px;
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-  border-radius: 2px;
-}
 </style>
